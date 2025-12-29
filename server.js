a66af13e4691f54154096dfccc7fa95adbe62e13
luckyv08 <luvkushv2004@gmail.com>
2025-12-29 14:56:11 +0530

    server.js

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MySQL connection (Railway)
+// ✅ MySQL connection (Railway / Render)
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
